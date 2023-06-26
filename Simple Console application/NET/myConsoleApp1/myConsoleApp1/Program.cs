dd7170006d618f8e792b52c403055eefd4da8466
Harshana Suraweera <37546163+harshana1@users.noreply.github.com>
2023-06-26 14:09:10 +0530

    Add files via upload

---
+++ Simple Console application/NET/myConsoleApp1/myConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] marks;
+            marks = new int[5];*/
+
+            int[] marks = new int[5];
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.Write("Enter Marks : ");
+                marks[i] = Int32.Parse(Console.ReadLine());
+            
+            }
+           /* Console.WriteLine(marks[0]);
+            Console.WriteLine(marks[1]);
+            Console.WriteLine(marks[2]);
+            Console.WriteLine(marks[3]);
+            Console.WriteLine(marks[4]);
+            Console.WriteLine(marks[5]);*/
+
+            for(int j = 0; j < 5; j++) 
+            {
+                Console.WriteLine(marks[j]);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
